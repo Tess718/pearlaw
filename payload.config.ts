fb0a3f5668a7f409128b3d4f4a9b28703ddbfd44
Liasu Teslim <teslimliasu49@gmail.com>
2025-12-20 13:48:07 +0100

    image db fix

--- payload.config.ts
+++ payload.config.ts
@@ -4,6 +4,7 @@ import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 import { Users } from './app/(payload)/collections/Users'
 import { Media } from './app/(payload)/collections/Media'
@@ -28,5 +29,21 @@ export default buildConfig({
     url: process.env.DATABASE_URI || '',
   }),
   sharp,
-  plugins: [],
+  plugins: [
+    s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: process.env.S3_BUCKET || '',
+      config: {
+        endpoint: process.env.S3_ENDPOINT,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+        },
+        region: process.env.S3_REGION || '',
+        forcePathStyle: true, // Needed for Supabase/MinIO usually
+      },
+    }),
+  ],
 })
